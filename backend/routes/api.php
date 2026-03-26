4ac86186bb00932421a3f1da0605e54daecebdfd
brown-bas <majorbarnu@gmail.com>
2026-03-26 01:59:58 +0100

    feat(routes): add api resource routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\CouponController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\PickupController;
+use App\Http\Controllers\PublisherController;
+use App\Http\Controllers\ReceiptController;
+use App\Http\Controllers\WishlistController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource("/books", BookController::class);
+Route::apiResource('authors', AuthorController::class);
+Route::apiResource('publishers', PublisherController::class);
+Route::apiResource('genres', GenreController::class);
+Route::apiResource('books', BookController::class);
+Route::apiResource('wishlists', WishlistController::class);
+Route::apiResource('receipts', ReceiptController::class);
+Route::apiResource('coupons', CouponController::class);
+Route::apiResource('pickups', PickupController::class);
