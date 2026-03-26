0c1a46d83a7d69a4034c8f6a2ff8b3c9a51883a5
brown-bas <majorbarnu@gmail.com>
2026-03-26 01:28:01 +0100

    fix(resources): fix receipt books response

--- backend/app/Http/Resources/ReceiptResource.php
+++ backend/app/Http/Resources/ReceiptResource.php
@@ -18,7 +18,19 @@ class ReceiptResource extends JsonResource
             'id' => $this->id,
             'date' => $this->date,
             'user' => new UserResource($this->whenLoaded('user')),
-            'books' => BookResource::collection($this->whenLoaded('books')),
+            'books' => $this->whenLoaded('books', fn() => $this->books->map(fn($book) => [
+                'id' => $book->id,
+                'img_path' => $book->img_path,
+                'title' => $book->title,
+                'price' => $book->price,
+                'pages' => $book->pages,
+                'stock' => $book->stock,
+                'quantity' => $book->pivot->quantity,
+                'price_at_purchase' => $book->pivot->price_at_purchase,
+                'authors' => AuthorResource::collection($book->authors),
+                'publisher' => new PublisherResource($book->publisher),
+                'genre' => new GenreResource($book->genre),
+            ])),
             'coupons' => CouponResource::collection($this->whenLoaded('coupons')),
             'pickup' => new PickupResource($this->whenLoaded('pickup'))
         ];
