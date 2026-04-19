9e71c70434806db8223ac256e19bf1aae6ade93e
brown-bas <majorbarnu@gmail.com>
2026-04-19 14:53:21 +0200

    feat(home): add random-category and discounted sections

--- frontend/src/routes/(public)/+page.server.ts
+++ frontend/src/routes/(public)/+page.server.ts
@@ -2,11 +2,18 @@ import type { PageServerLoad } from './$types';
 import { API_URL } from '$env/static/private';
 import * as m from '$lib/paraglide/messages.js';
 
-export const load: PageServerLoad = async () => {
-	const { data } = await fetch(`${API_URL}/books/top-purchased`).then((x) => x.json());
-
+export const load: PageServerLoad = async ({ cookies }) => {
 	return {
-		books: data,
+		topPurchased: (await fetch(`${API_URL}/books/top-purchased`).then((x) => x.json())).data,
+		randomCategory: (await fetch(`${API_URL}/books/random-category`).then((x) => x.json())).data,
+		discounted: (
+			await fetch(`${API_URL}/books/discounted`, {
+				headers: {
+					Authorization: cookies.get('auth_token') ? `Bearer ${cookies.get('auth_token')}` : '',
+					'X-Requested-With': 'XMLHttpRequest'
+				}
+			}).then((x) => x.json())
+		).data,
 		title: m['title.home']()
 	};
 };
--- frontend/src/routes/(public)/+page.svelte
+++ frontend/src/routes/(public)/+page.svelte
@@ -4,7 +4,15 @@
 	const { data } = $props();
 </script>
 
+<section class="flex h-max w-full flex-col items-center gap-6 px-12 py-20 md:items-start">
+	<h1 class="text-2xl">{m['home.top_10']()}</h1>
+	<BookCarousel books={data.topPurchased} topList={true} discounts={data.discounts} />
+</section>
+<section class="flex h-max w-full flex-col items-center gap-6 px-12 py-20 md:items-start">
+	<h1 class="text-2xl">{m['home.random_category']()}</h1>
+	<BookCarousel books={data.randomCategory} discounts={data.discounts} />
+</section>
 <section class="flex h-max w-full flex-col items-center gap-6 px-12 pt-20 pb-48 md:items-start">
-	<h1 class="text-2xl">{m['title.home']()}</h1>
-	<BookCarousel books={data.books} topList={true} discounts={data.discounts} />
+	<h1 class="text-2xl">{m['home.discounted']()}</h1>
+	<BookCarousel books={data.discounted} discounts={data.discounts} />
 </section>
