10fe11eec8319f668426d052508016f9974138c3
brown-bas <majorbarnu@gmail.com>
2026-04-23 19:24:53 +0200

    feat(i18n): change to shadcn select component and add icon for easier use

--- frontend/src/lib/components/LocaleSwitcher.svelte
+++ frontend/src/lib/components/LocaleSwitcher.svelte
@@ -1,22 +1,35 @@
 <script lang="ts">
 	import { getLocale, locales, setLocale } from '$lib/paraglide/runtime';
+	import * as Select from '$lib/components/ui/select';
+	import { Globe } from '@lucide/svelte';
 
 	let {
 		disabled = false,
-		classes = 'border-0 bg-transparent disabled:pointer-events-none disabled:opacity-50'
+		classes = 'border-0 bg-transparent! disabled:pointer-events-none disabled:opacity-50'
 	} = $props();
 
-	let currentLocale = $state(getLocale());
+	let currentLocale = $state<string>(getLocale());
+
+	function handleChange(value: string) {
+		setLocale(value as (typeof locales)[number]);
+		currentLocale = value;
+	}
 </script>
 
 {#key currentLocale}
-	<select
-		onchange={(e) => setLocale(e.currentTarget.value as (typeof locales)[number])}
-		class={classes}
-		{disabled}
-	>
-		{#each locales as locale}
-			<option selected={locale === currentLocale}>{locale.toUpperCase()}</option>
-		{/each}
-	</select>
+	<Select.Root type="single" value={currentLocale} onValueChange={handleChange} {disabled}>
+		<Select.Trigger class={classes}>
+			<div class="flex flex-row items-center gap-2">
+				<Globe />
+				{currentLocale.toUpperCase()}
+			</div>
+		</Select.Trigger>
+		<Select.Content>
+			{#each locales as locale}
+				<Select.Item class="cursor-pointer" value={locale} disabled={locale === currentLocale}>
+					{locale.toUpperCase()}
+				</Select.Item>
+			{/each}
+		</Select.Content>
+	</Select.Root>
 {/key}
