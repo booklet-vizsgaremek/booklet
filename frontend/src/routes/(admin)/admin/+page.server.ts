070f5434f80ac32d59fb374a04e6aee5e56c01af
brown-bas <majorbarnu@gmail.com>
2026-04-23 19:31:58 +0200

    feat(admin): add dashboard and move user management to another location

--- frontend/src/routes/(admin)/admin/+page.server.ts
+++ frontend/src/routes/(admin)/admin/+page.server.ts
@@ -1,22 +1,6 @@
+import * as m from '$lib/paraglide/messages';
 import type { PageServerLoad } from './$types';
-import * as m from '$lib/paraglide/messages.js';
-import { API_URL } from '$env/static/private';
-import { getLocale } from '$lib/paraglide/runtime';
 
-export const load: PageServerLoad = async ({ fetch, url, locals, cookies }) => {
-	const filters = url.searchParams.size ? `?${url.searchParams}` : '';
-
-	const res = await fetch(`${API_URL}/users${filters}`, {
-		headers: {
-			'X-Requested-With': 'XMLHttpRequest',
-			Authorization: `Bearer ${cookies.get('auth_token')}`,
-			'X-Locale': getLocale()
-		}
-	}).then((x) => x.json());
-
-	return {
-		title: m['title.users'](),
-		users: res.data,
-		meta: res.meta
-	};
+export const load: PageServerLoad = async () => {
+	return { title: m['title.dashboard']() };
 };
--- frontend/src/routes/(admin)/admin/+page.svelte
+++ frontend/src/routes/(admin)/admin/+page.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
 	import getGreeting from '$lib/utils/greeting';
+	import * as m from '$lib/paraglide/messages.js';
+	import { goto } from '$app/navigation';
 	import { page } from '$app/state';
-	import DataTable from '$lib/components/users/data-table.svelte';
-	import type { User } from '$lib/types';
-
-	let {
-		data
-	}: {
-		data: {
-			users: User[];
-			meta: { per_page: number; total: number };
-		};
-	} = $props();
 </script>
 
 <div class="mx-auto flex w-full flex-col gap-8 px-4 pt-16! pb-12 md:w-4/5 md:px-0 md:pb-24">
 	<h1 class="text-3xl">{getGreeting(page.data.user?.first_name ?? null)}</h1>
-	<DataTable {data} />
+	<h2 class="text-2xl">{m['management']()}</h2>
+	<div class="flex flex-col gap-4 md:flex-row">
+		<Button onclick={() => goto('/admin/users')} class="cursor-pointer">
+			{m['title.users']()}
+		</Button>
+		<Button onclick={() => goto('/staff/orders')} class="cursor-pointer">
+			{m['title.orders']()}
+		</Button>
+		<Button onclick={() => goto('/books')} class="cursor-pointer">
+			{m['title.book_lookup']()}
+		</Button>
+	</div>
 </div>
