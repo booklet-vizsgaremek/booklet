607c765941d276e3c13f0a748c19cb7c3bcdc195
brown-bas <majorbarnu@gmail.com>
2026-04-25 05:37:24 +0200

    feat(files): add /files route to serve public storage files

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/files/{folder}/{filename}', function (string $folder, string $filename) {
+    $path = storage_path("app/public/{$folder}/{$filename}");
+    abort_unless(file_exists($path), 404);
+    return response()->file($path);
 });
