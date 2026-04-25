7d448b4fc630e86e0c300960631562561c3ecbb9
brown-bas <majorbarnu@gmail.com>
2026-04-26 01:17:22 +0200

    feat(layout): extend guard to admins, prefetch publishers, genres, and authors

--- frontend/src/routes/(manager)/+layout.server.ts
+++ frontend/src/routes/(manager)/+layout.server.ts
@@ -1,6 +1,13 @@
 import { error } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
+import { API_URL } from '$env/static/private';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
-	if (!locals.user || locals.user.role !== 'manager') return error(403);
+	if (!locals.user || !['manager', 'admin'].includes(locals.user.role)) return error(403);
+
+	return {
+		publishers: (await fetch(`${API_URL}/publishers`).then((x) => x.json())).data,
+		genres: (await fetch(`${API_URL}/genres`).then((x) => x.json())).data,
+		authors: (await fetch(`${API_URL}/authors`).then((x) => x.json())).data
+	};
 };
